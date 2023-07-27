dbf1698275b9c94a3828d58cf301942008d3295a
Martin Chapman <contact@martinchapman.co.uk>
2023-07-27 21:47:56 +0100

    Add TSA interactions

--- src/ink/ink.py
+++ src/ink/ink.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 import logging, os, base64, json
 from typing import cast
 from cryptography.hazmat.primitives import hashes
@@ -5,6 +6,7 @@ from cryptography.hazmat.primitives.asymmetric import padding
 from cryptography.hazmat.primitives.serialization import load_pem_private_key
 from cryptography.x509 import load_pem_x509_certificate
 from cryptography.hazmat.backends.openssl.rsa import RSAPublicKey
+from rfc3161ng import RemoteTimestamper, get_timestamp  # type: ignore
 
 from ink.bard import Bard
 from ink.ink_types import Chatbot, InkMessage
@@ -49,3 +51,31 @@ class Ink:
             return True
         except:
             return False
+
+    def getTimestamp(self, signedMessages: str) -> str:
+        timestamp: str = base64.b64encode(
+            RemoteTimestamper(
+                'https://freetsa.org/tsr',
+                certificate=open(os.environ['tsaCertificatePath'], 'rb').read(),
+                hashname='sha512',
+            ).timestamp(data=base64.b64decode(signedMessages.encode('utf-8')))
+        ).decode('utf-8')
+        self.__logger.debug(timestamp)
+        return timestamp
+
+    def verifyTimestamp(self, signedMessages: str, timestamp: str) -> bool:
+        try:
+            RemoteTimestamper(
+                'https://freetsa.org/tsr',
+                certificate=open(os.environ['tsaCertificatePath'], 'rb').read(),
+                hashname='sha512',
+            ).check(
+                base64.b64decode(timestamp.encode('utf-8')),
+                data=base64.b64decode(signedMessages.encode('utf-8')),
+            )
+            return True
+        except:
+            return False
+
+    def extractTime(self, timestamp: str) -> datetime:
+        return get_timestamp(base64.b64decode(timestamp.encode('utf-8')))
--- tests/test_ink.py
+++ tests/test_ink.py
@@ -1,8 +1,11 @@
 import base64
-from ink.ink_types import InkMessage
+from zoneinfo import ZoneInfo
 import pytest  # type: ignore
+from datetime import datetime
 from dotenv import load_dotenv
+
 from ink.ink import Ink
+from ink.ink_types import InkMessage
 
 
 @pytest.fixture(scope='session', autouse=True)
@@ -10,14 +13,66 @@ def load_env() -> None:
     load_dotenv()
 
 
-def test_Ink() -> None:
+def test_sendMessage() -> None:
     ink: Ink = Ink()
     message: str = ink.sendMessage('hello world')
     assert type(message) == str and len(message) > 0
-    signedMessages: str = ink.signMessages([InkMessage(sender='bob', text=message)])
+
+
+def test_signMessage() -> None:
+    ink: Ink = Ink()
+    signedMessages: str = ink.signMessages(
+        [InkMessage(sender='bob', text='hello world')]
+    )
     assert len(signedMessages) > 0
-    assert ink.verifySignature(signedMessages, [InkMessage(sender='bob', text=message)])
+
+
+def test_verifySignature() -> None:
+    ink: Ink = Ink()
+    signedMessages: str = ink.signMessages(
+        [InkMessage(sender='bob', text='hello world')]
+    )
+    assert len(signedMessages) > 0
+    assert ink.verifySignature(
+        signedMessages, [InkMessage(sender='bob', text='hello world')]
+    )
     assert not ink.verifySignature(
         base64.b64encode(bytes('foo', 'utf-8')).decode('utf-8'),
-        [InkMessage(sender='bob', text=message)],
+        [InkMessage(sender='bob', text='hello world')],
     )
+
+
+def test_getTimestamp() -> None:
+    ink: Ink = Ink()
+    signedMessages: str = ink.signMessages(
+        [InkMessage(sender='bob', text='hello world')]
+    )
+    assert len(signedMessages) > 0
+    assert len(ink.getTimestamp(signedMessages)) > 0
+
+
+def test_verifyTimestamp() -> None:
+    ink: Ink = Ink()
+    signedMessages: str = ink.signMessages(
+        [InkMessage(sender='bob', text='hello world')]
+    )
+    assert len(signedMessages) > 0
+    timestamp: str = ink.getTimestamp(signedMessages)
+    assert len(timestamp) > 0
+    assert ink.verifyTimestamp(signedMessages, timestamp)
+    assert not ink.verifyTimestamp(
+        base64.b64encode(bytes('foo', 'utf-8')).decode('utf-8'), timestamp
+    )
+
+
+def test_extractTime() -> None:
+    ink: Ink = Ink()
+    signedMessages: str = ink.signMessages(
+        [InkMessage(sender='bob', text='hello world')]
+    )
+    assert len(signedMessages) > 0
+    timeNow: datetime = datetime.now(tz=ZoneInfo('UTC'))
+    timestamp: str = ink.getTimestamp(signedMessages)
+    assert len(timestamp) > 0
+    timestampTime: datetime = ink.extractTime(timestamp)
+    assert timestampTime.hour == timeNow.hour and timestampTime.minute == timeNow.minute
